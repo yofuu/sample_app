34b662f6e66136bcc749f1706f6557ac7217d831
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2022-08-30 10:14:28 +0000

    [Fourth commit]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'top' => 'homes#top'
   get 'lists/:id' => 'lists#show', as: 'list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
